a80312be1c72139ac7d5067e0e7f6d3e82c1efdd
Alexandre Bigot <abigot@sbgat402.in2p3.fr>
2022-12-15 10:46:07 +0100

    Fix typos

--- PWGHF/TableProducer/candidateSelectorB0ToDPi.cxx
+++ PWGHF/TableProducer/candidateSelectorB0ToDPi.cxx
@@ -50,9 +50,9 @@ struct HfCandidateSelectorB0ToDPi {
 
   // Apply topological cuts as defined in SelectorCuts.h; return true if candidate passes all cuts
   template <typename T1, typename T2, typename T3>
-  bool selectionTopol(const T1& hfCandB0, const T2& hfCandD, const T3& trackPi)
+  bool selectionTopol(const T1& candB0, const T2& candD, const T3& trackPi)
   {
-    auto candpT = hfCandB0.pt();
+    auto candpT = candB0.pt();
     int pTBin = findBin(binsPt, candpT);
     if (pTBin == -1) {
       // Printf("B0 topol selection failed at getpTBin");
@@ -65,7 +65,7 @@ struct HfCandidateSelectorB0ToDPi {
     }
 
     // B0 mass cut
-    if (std::abs(invMassB0ToDPi(hfCandB0) - RecoDecay::getMassPDG(pdg::Code::kB0)) > cuts->get(pTBin, "m")) {
+    if (std::abs(invMassB0ToDPi(candB0) - RecoDecay::getMassPDG(pdg::Code::kB0)) > cuts->get(pTBin, "m")) {
       // Printf("B0 topol selection failed at mass diff check");
       return false;
     }
@@ -75,72 +75,71 @@ struct HfCandidateSelectorB0ToDPi {
       return false;
     }
 
-    // D- pt
-    if (hfCandD.pt() < cuts->get(pTBin, "pT D^{#minus}")) {
+    // D pt
+    if (candD.pt() < cuts->get(pTBin, "pT D")) {
       return false;
     }
 
     // D mass cut
     // if (trackPi.sign() > 0) {
-    //   if (std::abs(InvMassDplus(hfCandD) - RecoDecay::getMassPDG(pdg::Code::kDMinus)) > cuts->get(pTBin, "DeltaM")) {
+    //   if (std::abs(InvMassDplus(candD) - RecoDecay::getMassPDG(pdg::Code::kDMinus)) > cuts->get(pTBin, "DeltaM")) {
     //     return false;
     //   }
     // }
 
     // B0 Decay length
-    if (hfCandB0.decayLength() < cuts->get(pTBin, "B0 decLen")) {
+    if (candB0.decayLength() < cuts->get(pTBin, "B0 decLen")) {
       return false;
     }
 
     // B0 Decay length XY
-    if (hfCandB0.decayLengthXY() < cuts->get(pTBin, "B0 decLenXY")) {
+    if (candB0.decayLengthXY() < cuts->get(pTBin, "B0 decLenXY")) {
       return false;
     }
 
     // B0 chi2PCA cut
-    if (hfCandB0.chi2PCA() > cuts->get(pTBin, "Chi2PCA")) {
+    if (candB0.chi2PCA() > cuts->get(pTBin, "Chi2PCA")) {
       // Printf("B0 selection failed at chi2PCA");
       return false;
     }
 
     // B0 CPA cut
-    if (hfCandB0.cpa() < cuts->get(pTBin, "CPA")) {
+    if (candB0.cpa() < cuts->get(pTBin, "CPA")) {
       return false;
     }
 
     // d0 of pi
-    if (std::abs(hfCandB0.impactParameter1()) < cuts->get(pTBin, "d0 Pi")) {
+    if (std::abs(candB0.impactParameter1()) < cuts->get(pTBin, "d0 Pi")) {
       return false;
     }
 
     // d0 of D
-    if (std::abs(hfCandB0.impactParameter0()) < cuts->get(pTBin, "d0 D")) {
+    if (std::abs(candB0.impactParameter0()) < cuts->get(pTBin, "d0 D")) {
       return false;
     }
 
     return true;
   }
 
-  void process(aod::HfCandB0 const& hfCandB0s, soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi> const&, aod::BigTracksPID const&)
+  void process(aod::HfCandB0 const& candB0s, soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi> const&, aod::BigTracksPID const&)
   {
-    for (auto const& hfCandB0 : hfCandB0s) { // looping over B0 candidates
+    for (const auto& candB0 : candB0s) { // looping over B0 candidates
 
       int statusB0 = 0;
 
       // check if flagged as B0 → D- π+
-      if (!TESTBIT(hfCandB0.hfflag(), hf_cand_b0::DecayType::B0ToDPi)) {
+      if (!TESTBIT(candB0.hfflag(), hf_cand_b0::DecayType::B0ToDPi)) {
         hfSelB0ToDPiCandidate(statusB0);
         // Printf("B0 candidate selection failed at hfflag check");
         continue;
       }
 
       // D is always index0 and pi is index1 by default
-      // auto candD = hfCandD.prong0();
-      auto candD = hfCandB0.prong0_as<soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi>>();
-      auto trackPi = hfCandB0.prong1_as<aod::BigTracksPID>();
+      auto candD = candB0.prong0_as<soa::Join<aod::HfCand3Prong, aod::HfSelDplusToPiKPi>>();
+      auto trackPi = candB0.prong1_as<aod::BigTracksPID>();
 
       // topological cuts
-      if (!selectionTopol(hfCandB0, candD, trackPi)) {
+      if (!selectionTopol(candB0, candD, trackPi)) {
         hfSelB0ToDPiCandidate(statusB0);
         // Printf("B0 candidate selection failed at selection topology");
         continue;
